610a1ccc30b63f41b16b89d5b7ed1a20990b21d9
juhwisang <juhs2005@gmail.com>
2022-06-24 23:21:20 +0900

    add_run

--- run.py
+++ run.py
@@ -1,16 +1,37 @@
 from app.scanner import scan
 from app.attacker import xss
 from app.multidriver import Pool
+import argparse
 
-target = "https://0ab10010033cd79bc00553c1007a00ea.web-security-academy.net/"
-# target = "https://xss-game.appspot.com/level2/frame"
 
-pool = Pool(3)
-try:
-    links = scan(target, driver_pool=pool)
-    print("scan result:", links)
-    succeed = xss(links, driver_pool=pool)
-    print("XSS result:", succeed)
-finally:
-    pool.quit()
-    ...
+def get_args():
+    parser = argparse.ArgumentParser(description="To scan, and xss attack.")
+    parser.add_argument("target", help="Target website")
+    parser.add_argument("--driver-pool-size", type=int, help="Set Driver Pool size.", default=3)
+    parser.add_argument("--no-js", const=True, action="store_const", help="Use 'requests' instead 'selenium'.", default=False)
+    # parser.add_argument("--xss-cheat-sheet", help="Set a cheat sheet file for xss.")
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+    target = args.target
+    driver_pool_size = args.driver_pool_size
+    use_js_execution = not args.no_js
+
+    if use_js_execution:
+        pool = Pool(driver_pool_size)
+    else:
+        pool = None
+
+    try:
+        links = scan(target, driver_pool=pool, js_execution=use_js_execution)
+        print("scan result:", links)
+        succeed = xss(links, driver_pool=pool, js_execution=use_js_execution)
+        print("XSS result:", succeed)
+    finally:
+        if use_js_execution:
+            pool.quit()
+
+if __name__ == "__main__":
+    main()
